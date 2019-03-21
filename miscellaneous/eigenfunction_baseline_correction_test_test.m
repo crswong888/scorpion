9d7308a842b34e42d597e4c935323f2d4b151e8a
crswong888 <crswong888@gmail.com>
2019-03-21 19:36:54 -0400

    Changed back to the null space solver, but only solved null space for odd integers of n, which are exact solutions. Introduced test acceleration time history into this code. Implemented the Fourier-Euler transform procedure to solve for V(t) and D(t). This code is still far from complete and is likely invalid, but its a big step forward.

--- miscellaneous/eigenfunction_baseline_correction_test_test.m
+++ miscellaneous/eigenfunction_baseline_correction_test_test.m
@@ -1,23 +1,36 @@
 clear all
 
-N = 5 ;
-T = 5 ;
+N = 11 ;
+T = 0.05 ;
 
-dt = 0.1 ;
+dt = 1E-4 ;
 t = 0 : dt : T ;
 t_N = length(t) ; 
 
 SLE = zeros(6,6,N) ;
-C = rand(6,1,N) ;
+C = zeros(6,1,N) ;
 homg = zeros(6,1) ;
 
-N_eqn = size(C,1) ;
-normVal = Inf ;
-tol = 1E-8 ;
-Gauss_iter = 0 ;
-max_iter = 50 ;
+beta = 1/4 ;
+gamma = 1/2 ;
+
+u_dot_dot(1) = 0 ;
+u_dot(1) = 0 ;
+u_(1) = 0 ;
+
+for i = 1:t_N - 1
+    if t(i) < 0.05 - dt
+        u_dot_dot(i+1) = - (200 * pi)^2 * sin(200 * pi * t(i+1)) ;
+    else
+        u_dot_dot(i+1) = 0 ;
+    end
+    u_dot(i+1) = u_dot(i) + (1-gamma) * dt * u_dot_dot(i) + gamma * dt * u_dot_dot(i+1) ;
+    u_(i+1) = u_(i) + dt * u_dot(i) + (0.5 - beta) * dt * dt * u_dot_dot(i) + beta * dt * dt * u_dot_dot(i+1) ;
+end
 
 for n = 1:N
+    
+    if rem(n,2) ~= 0
     
     v(n) = (n + 1) * pi ;
     
@@ -44,31 +57,8 @@ for n = 1:N
                   
                   -2*cos(v(n)) -2*sin(v(n)) -e_plus(n)*SC_min(n) ...
                     e_plus(n)*CS_plus(n) e_min(n)*SC_plus(n) -e_min(n)*CS_min(n) ] ;
-    
-    C = rand(6,1,n) ;
-    
-    while normVal > tol
-        
-        C_old = C ;
-        
-        for i = 1:N_eqn
-            
-            Sigma = 0 ;
-            
-            for j=1:i-1
-                
-                Sigma = Sigma + SLE(i,j,n) * C(j) ;
-                
-            end
-            
-            C(i,n) = ( 1 / SLE(i,i,n) ) * ( homg(i) - Sigma ) ;
-            
-        end
-        
-        Gauss_iter = Gauss_iter + 1 ;
-        normVal = norm(C_old(:,:,n) - C(:,:,n)) ;
-        
-    end
+       
+    C(:,:,n) = null(SLE(:,:,n)) ;
     
     lambda(n) = v(n) / T ;
     
@@ -95,13 +85,57 @@ for n = 1:N
             + sqrt(3) * sin(lambda(n) * t(i) / 2) ) + C(6,n) * ( -sqrt(3) ...
             * cos(lambda(n) * t(i) / 2) + sin(lambda(n) * t(i) / 2) ) ) ) ;
         
-     end
+        Psi2_dot_dot(n,i) = Psi_dot_dot(n,i)^2 ;
+       
+    end
      
+    end
+end
+
+A = u_dot_dot ;
+a = zeros(n,1) ;
+integral = zeros(n,1) ;
+
+for i = 1:t_N - 2
+    x_i = i * T / t_N ;
+    x_i_plus_1 = (i + 1) * T / t_N ;
+    for n = 1:N
+        if rem(n,2) ~= 0
+            integral(n) = 0.5 * sum((x_i_plus_1 - x_i)*(Psi2_dot_dot(n,i+1) + Psi2_dot_dot(n,i))) ;
+            a(n) = 1 / integral(n) * sum(A(i)*Psi_dot_dot(n,i))*dt ;
+        end
+    end
+end
+
+for n = 1:N
+    for i = 1:t_N 
+        V(i) = sum(a(n)*Psi_dot(n,i)) ;
+        D(i) = sum(a(n)*Psi(n,i)) ;
+    end
+end
+
+for i = 1:2*t_N-1
+    if i < 501
+        A(i) = A(i) ;
+        V(i) = V(i) ;
+        D(i) = D(i) ;
+    else
+        A(i) = 0 ;
+        V(i) = 0 ;
+        D(i) = 0 ;
+    end
 end
 
 figure(1)
-plot(t,Psi(N,:))
+plot(0:dt:0.1,A)
 figure(2)
-plot(t,Psi_dot(N,:))
+plot(0:dt:0.1,V)
 figure(3)
-plot(t,Psi_dot_dot(N,:))
+plot(0:dt:0.1,D)
+
+% figure(4)
+% plot(t,Psi(:,:))
+% figure(5)
+% plot(t,Psi_dot(:,:))
+% figure(6)
+% plot(t,Psi_dot_dot(:,:))
