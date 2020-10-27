8a7765901cc15d9da55f392b409c02bff653efa6
crswong888 <crswong888@gmail.com>
2020-10-26 22:12:07 -0600

    Cleaned up plot code a bit

--- miscellaneous/matlab/felab/felab/CPS4_test.m
+++ miscellaneous/matlab/felab/felab/CPS4_test.m
@@ -112,163 +112,7 @@ F = assembleGlobalForce(num_dofs, num_eqns, real_idx_diff, forces);
 %%% I might need to seriously increase the load for this problem so I don't have to apply such a
 %%% huge scale factor. Or switch up the mat/geo props so its not so stiff, either or
 
-%%% once all of the features are in the plot, e.g., colorbar, title, and axis labels, then we need
-%%% to generate this test plot with all of those objects and finally get the axis resolution
-figure('Visible', 'off', 'Units', 'normalize', 'Position', [0.125, 0.25, 0.75, 0.75])
-ax = axes('Visible', 'off', 'Position', [0.05, 0.05, 0.9, 0.9]);
-set(ax, 'Units', 'pixels');
-resolution = [max(ax.Position(3:4)); min(ax.Position(3:4))];
-close all
-
-num_nodes = length(nodes{:,1});
-num_dims = length(nodes{1,2:end});
-num_blocks = 1;
-num_local_nodes = 4; % length(ele_blk{b}{1,:}) / (num_dims + 1);
-num_elems = length(mesh(:,1)); % length(ele_blk{b}{:,1});
-
-ele_blk = {mesh};
-
-% you can just put 0 here and get undisplaced mesh - default value will be 1
-scale = 250;
-
-displaced_nodes = zeros(num_nodes, num_dims);
-disp_mag = zeros(num_nodes, 1);
-for i = 1:num_nodes
-    idx = num_dofs * (i - 1) + [1; 2];
-    real_idx = idx - real_idx_diff(idx);
-    displaced_nodes(i,:) = nodes{i,([1, 2] + 1)} + transpose(scale * Q(real_idx));
-    disp_mag(i) = norm(Q(real_idx));
-end
-
-% % append dim ids to extents so if we swap x and y we know which is which
-% extents = cat(1, transpose(1:2), transpose(range(displaced_nodes(:,1:2))));
-
-%%% could've used range() here, but I need the min and max too for later calc, so keeping it
-extents = zeros(num_dims, 4);
-extents(:, 1) = 1:num_dims;
-for s = 1:num_dims
-    extents(s, 2) = min(displaced_nodes(:,s)); % min(nodes{:,(s + 1)});
-    extents(s, 3) = max(displaced_nodes(:,s)); % max(nodes{:,(s + 1)});
-end
-extents(:,4) = extents(:,3) - extents(:,2);
-extents = sortrows(extents, 4, 'descend');
-
-%%% because of scaling, this whole fancy grid system concept is officially obsolete. Damn.
-%%% what I can do is show "Real Displacements" in the contour plot and legend, and perhaps label the
-%%% title appropriately. But the spatial coordinates will reflect values as if the displacements
-%%% were actually those scaled values. Theres really no simple way around this.
-
-lowest = Inf;
-for i = 10:20
-    %/ some magnitude that is a multiple of 5 is best, 2 is okay, and 1 not best
-    for m = [5, 2, 1]
-        commdom = m * 10^(sign(log10(extents(1,4) / i)) * floor(abs(log10(extents(1,4) / i)) + 2));
-        remainder = abs(extents(1,4) - round(extents(1,4) / i / commdom) * commdom * i);
-        if (remainder < lowest)
-            lowest = remainder;
-            dx = round(extents(1,4) / i / commdom) * commdom;
-            Nx = i;
-        end
-    end
-end
-
-%%% need to only do this max thing for y, otherwise, my assumptions here may be invalid. Plus, this
-%%% is going to be a 2D plotter, so its fine
-offset = max(dx, abs(((extents(1,4) + 2 * dx) .* resolution / resolution(1) - extents(:,4)) / 2));
-limits(:,1) = extents(:,2) - offset;
-limits(:,2) = extents(:,3) + offset;
-
-grid = {(round(limits(1,1) / dx) * dx - 10 * dx):dx:(round(limits(1,2) / dx) * dx + 10 * dx);
-        (round(limits(2,1) / dx) * dx - 10 * dx):dx:(round(limits(2,2) / dx) * dx + 10 * dx)};
-
-%%% damn, this is why natural coordinate maps are dope. How do I select interpolation points in the
-%%% deformed space of the element?? You hardly can. I mean, you could come up with some method, but
-%%% I already have one implemented - lagrange spatial interpolations. The points can be selected as
-%%% a uniform grid on xi \in [-1, 1], and then those points can be mapped into deformed space
-
-%%% my linear interpolation function will be useful for interpolating the colorbar, with extrapolate
-%%% as false, because I'm gonna set the color bar limits to max and min displacements of course.
-%%% however, I still shall interpolate using my shape functions. actually maybe just let matlab
-%%% plot object handle this job
-
-
-%%% be sure to have parameter for number of sample points
-    
-%// get element connectivity lines on each block
-connectivity = cell(1, num_blocks);
-for b = 1:num_blocks
-    connectivity{b} = zeros((num_local_nodes + 1), num_dims, num_elems);
-    for e = 1:num_elems
-        idx = mesh(e,1:3:end);
-        connectivity{b}(:,:,e) = [displaced_nodes(idx,1:2); displaced_nodes(idx(1),1:2)];
-    end
-end
-
-%/ creates a 3x3 array in 2D element (default shall be 10)
-data_pts = 3;
-
-%%% might even need a different function for beam plots specificially because the spatial
-%%% interpolation is quite different
-
-%%% line interpolation, quad interpolation, hex interpolation, ...
-
-u = cell(1, num_blocks);
-v = cell(1, num_blocks);
-qnorm = cell(1, num_blocks);
-x = cell(1, num_blocks);
-y = cell(1, num_blocks);
-
-dxi = 2 / (data_pts - 1);
-xi = -1:dxi:1;
-eta = -1:dxi:1;
-
-for b = 1:num_blocks
-    u{b} = zeros(data_pts, data_pts, num_elems); % num_elems{b}
-    v{b} = zeros(data_pts, data_pts, num_elems);
-    qnorm{b} = zeros(data_pts, data_pts, num_elems);
-    x{b} = zeros(data_pts, data_pts, num_elems);
-    y{b} = zeros(data_pts, data_pts, num_elems);
-    
-    q_idx = zeros(2 * num_local_nodes, 1);
-    u_idx = 1:2:length(q_idx);
-    v_idx = u_idx + 1;
-    for e = 1:num_elems %num_elems{b}
-        
-        nodeIDs = transpose(mesh(e,1:3:end));
-        coords = transpose([mesh(e,2:3:end); mesh(e,3:3:end)]);
-        
-        q_idx(u_idx) = num_dofs * (nodeIDs - 1) + 1;
-        q_idx(v_idx) = q_idx(u_idx) + 1;
-        q = Q(q_idx - real_idx_diff(q_idx));
-    
-        for i = 1:data_pts
-            for j = 1:data_pts       
-                N = evaluateCPS4ShapeFun(xi(i), eta(j));
-                
-                u{b}(i,j,e) = N * q(u_idx);
-                v{b}(i,j,e) = N * q(v_idx);
-                
-                qnorm{b}(i,j,e) = norm([u{b}(i,j,e), v{b}(i,j,e)]);
-                
-                %%% without connecting lines to all these points I won't be able to do solid
-                %%% coloring, so I'll have to figure that out too
-                
-                % get the original global coordinates and displace them
-                x{b}(i,j,e) = N * coords(:,1) + scale * u{b}(i,j,e);
-                y{b}(i,j,e) = N * coords(:,2) + scale * v{b}(i,j,e);                
-            end
-        end
-        
-    end
-
-end
-
 %%% perhaps all of the different plot object titles could be customized too, but def set defaults
-
-%%% allow edge color interpolation object to handle coloring between sample points so it doesn't
-%%% look patchy, assuming that it properly interpolates the jet scheme
-%%% I suppose if it works in a nice general way, that the color scheme could be an input parameter,
-%%% for example, the input could be 'jet(256)', but only if contours = true
 
 %%% if i get crazy and do a full 2 coloring and then like fill in colors with edge face colors and 
 %%% shit (i.e., same way the gradient works), I could include a 'surface', and then other ones too
@@ -293,107 +137,222 @@ end
 
 %%% the default plot style shall be 'surface with edges', which won't have any affect on 2-pointers
 
+%%% might even need a different function for beam plots specificially because the spatial
+%%% interpolation is quite different
 
-%%% I wonder if theres a way to determine current number of figure objects, so that I don't have to
-%%% close any plots and I can just say like figure(N) or something. Or maybe just use clf 'clf(1)'?
+%%% line interpolation, quad interpolation, hex interpolation, ...
 
+
+
+%%% temporary input params
+ele_blk = {mesh};
+nodes = nodes;
+scale = 250; % you can just put 0 here and get undisplaced mesh - default value will be 1
+data_pts = 3;
+% Component
+% element type
+% object titles
+% plot style
+% contours
+
+
+%// This process could potentially take more than a few moments, so let its user know its working.
 fprintf('Generating plot... ')
 
-figure(1)
-set(gcf, 'Units', 'normalize', 'Position', [0.125, 0.25, 0.75, 0.75])
-ax = axes('Position', [0.05, 0.05, 0.9, 0.9]);
+%// convenience variables
+num_nodes = length(nodes{:,1});
+num_blocks = length(ele_blk);
+num_elems = zeros(1, num_blocks);
+for b = 1:num_blocks
+    num_elems(b) = length(ele_blk{b}(:,1));
+end
 
-%/ plot nodes
-plot(displaced_nodes(:,1), displaced_nodes(:,2), 'o', 'markerfacecolor', 'b', 'markersize', 3.5)
+%// Retrieve nodal values and displace original mesh
+displaced_nodes = zeros(num_nodes, 2);
+field = zeros(num_nodes, 1);
+for i = 1:num_nodes
+    %/ Determine DOF positions in global displacement index
+    idx = num_dofs * (i - 1) + [1; 2];
+    real_idx = idx - real_idx_diff(idx);
+    
+    %/ apply scaled displacements to nodes and get their new positions
+    displaced_nodes(i,:) = nodes{i,([1, 2] + 1)} + transpose(scale * Q(real_idx));
+    
+    %/ get desired nodal displacement value
+    % if magnitude, else, field...
+    field(i) = norm(Q(real_idx));
+end
+
+%// interpolate displacement field through elements and get their nodal connectivity
+x = cell(1, num_blocks);
+y = cell(1, num_blocks);
+subfield = cell(1, num_blocks);
+connectivity = cell(1, num_blocks);
+
+%/ set up element interpolation grid in natural coordinate system
+dxi = 2 / (data_pts - 1);
+xi = -1:dxi:1;
+eta = -1:dxi:1;
+
+%/ loop through all blocks domains
+for b = 1:num_blocks
+    %/ initialize element data
+    x{b} = zeros(data_pts, data_pts, num_elems(b));
+    y{b} = zeros(data_pts, data_pts, num_elems(b));
+    subfield{b} = zeros(data_pts, data_pts, num_elems(b));
+    
+    %/ displacement index
+    q_idx = zeros(2 * length(ele_blk{b}(1,:)) / 3, 1);
+    u_idx = 1:2:length(q_idx);
+    v_idx = u_idx + 1;
+    
+    %/ loop through all elements on block
+    for e = 1:num_elems(b)
+        %/ global node IDs and coordinates
+        nodeIDs = transpose(mesh(e,1:3:end));
+        
+        % ---------
+        % if contours
+        coords = transpose([mesh(e,2:3:end); mesh(e,3:3:end)]);
+        
+        %/ retrieve the nodal displacements from global index
+        q_idx(u_idx) = num_dofs * (nodeIDs - 1) + 1;
+        q_idx(v_idx) = q_idx(u_idx) + 1;
+        q = Q(q_idx - real_idx_diff(q_idx));
+        
+        %/ use shape functions to interpolate nodal displacements to specified grid points
+        for i = 1:data_pts
+            for j = 1:data_pts
+                % evaluate shape functions and get displacement values
+                N = evaluateCPS4ShapeFun(xi(i), eta(j));
+                u = N * q(u_idx);
+                v = N * q(v_idx);
+                
+                % apply scaled displacements to grid points and get their new positions
+                x{b}(i,j,e) = N * coords(:,1) + scale * u;
+                y{b}(i,j,e) = N * coords(:,2) + scale * v;
+                
+                %/ get desired nodal displacement value
+                % if magnitude, else, subfield...
+                subfield{b}(i,j,e) = norm([u, v]);
+            end
+        end
+        
+        %/ get element connectivity lines on each block for wireframe and edges plots
+        connectivity{b}(:,:,e) = displaced_nodes([nodeIDs; nodeIDs(1)],:);
+    end
+end
+
+%// Generate a figure window with a nominal plot axes
+figure('Units', 'normalize', 'Position', [0.125, 0.25, 0.75, 0.75])
+ax = axes('Position', [0.05, 0.05, 0.9, 0.9], 'Layer', 'top');
+
+%/ plot nodes to at least intialize a plot space - show/hide them depending on plot style
+plot(displaced_nodes(:,1), displaced_nodes(:,2), 'o', 'markerfacecolor', 'w', 'markersize', 3.5)
 hold on
 
-%%% DAMN ima have to come up with my own tic for the colorbar lmao
-colormap jet(128)
-c = colorbar;
-ylabel(c, 'Real Displacement Magnitude') % or just displacement, depending on if scale is 1 or not
-%caxis([min(disp_mag), max(disp_mag)])
-
-xlim(limits(1,:))
-xticks(grid{1})
-ylim(limits(2,:))
-yticks(grid{2})
-set(gca,'Layer', 'top', 'CLim', [min(disp_mag), max(disp_mag)])
+% if points, need to add color to connectivity
 
 %/ loop through mesh blocks and plot each element
-plot(connectivity{1}(1,:,1), connectivity{1}(2,:,1))
+% ~points
 for b = 1:num_blocks
-    for e = 1:num_elems
+    for e = 1:num_elems(b)
         
-        %%% here's where I'll handle displacement colorings too
-        
-        %%% there's so much redundant data in this plot, which could potentially slow down the plot
-        %%% interactivity.
-        
+        % set contour colors for desired field component at each sample point
+        % if contours
         for i = 1:(data_pts - 1)
             for j = 1:(data_pts - 1)
-%                 sample_x = [x{b}(i,j,e);
-%                             x{b}((i + 1),j,e); 
-%                             x{b}((i + 1),(j + 1),e); 
-%                             x{b}(i,(j + 1),e);
-%                             x{b}(i,j,e)];
-% 
-%                 sample_y = [y{b}(i,j,e);
-%                             y{b}((i + 1),j,e); 
-%                             y{b}((i + 1),(j + 1),e); 
-%                             y{b}(i,(j + 1),e);
-%                             y{b}(i,j,e)];
-                        
+                % set up plot vertices in a closed polygon fashion so patch object can be used
                 sample_x = [x{b}(i,j,e),...
                             x{b}((i + 1),j,e),...
                             x{b}((i + 1),(j + 1),e),...
                             x{b}(i,(j + 1),e)];
-
+                        
                 sample_y = [y{b}(i,j,e),...
                             y{b}((i + 1),j,e),...
                             y{b}((i + 1),(j + 1),e),...
                             y{b}(i,(j + 1),e)];
                         
-                sample_val = [qnorm{b}(i,j,e),...
-                              qnorm{b}((i + 1),j,e),...
-                              qnorm{b}((i + 1),(j + 1),e),...
-                              qnorm{b}(i,(j + 1),e)];
+                sample_val = [subfield{b}(i,j,e),...
+                              subfield{b}((i + 1),j,e),...
+                              subfield{b}((i + 1),(j + 1),e),...
+                              subfield{b}(i,(j + 1),e)];
                 
+                % if a surface plot, color whole element, else, just edges
                 p = patch(sample_x, sample_y, 'k', 'Parent', ax);
                 set(p, 'CData', sample_val, 'FaceColor', 'interp', 'CDataMapping', 'scaled', 'EdgeColor', 'none');
-                
-                        
-                %plot(sample_x, sample_y, 'Color', 'r', 'Linewidth', 0.5)
             end
         end
         
-        plot(connectivity{b}(:,1,e), connectivity{b}(:,2,e), 'Color', 'b')
+        % if surface with edges or wireframe with no contours
+        plot(connectivity{b}(:,1,e), connectivity{b}(:,2,e), 'Color', 'w')
     end
 end
 
-%// set gradient background - I call this one "Shallow Ocean" XD
-bgx = [limits(1,1), limits(1,2), limits(1,2), limits(1,1)];
-bgy = [limits(2,1), limits(2,1), limits(2,2), limits(2,2)];
+%// define color mapping system for plot contours
+colormap jet(128)
+c = colorbar;
+ylabel(c, 'Real Displacement Magnitude')
+set(ax, 'CLim', [min(field), max(field)])
 
-%0.325, 0.545, 0.78 %0.40, 0.596, 0.804 (other nice monochrome colors)
-cdata(1,1,:) = [0.196, 0.396, 0.608];
+%/ set up plot space with a 1:1 ratio for both dimensions
+set(ax, 'Units', 'pixels');
+resolution = [max(ax.Position(3:4)); min(ax.Position(3:4))]; % current resolution of axes window
+set(ax, 'Units', 'normalize') % convert it back so window scaling continues to work
+
+% determine spatial extents of displaced mesh
+extents = zeros(2, 4);
+extents(:, 1) = 1:2;
+for s = 1:2
+    extents(s, 2) = min(displaced_nodes(:,s));
+    extents(s, 3) = max(displaced_nodes(:,s));
+end
+extents(:,4) = extents(:,3) - extents(:,2);
+extents = sortrows(extents, 4, 'descend');
+
+% determine a grid tick spacing for long dimension that conforms well to extents of displaced domain
+lowest = Inf;
+for i = 10:20 % minum of 10 increments and a maximum of 20
+    for m = [5, 2, 1] % some order of magnitude of a multiple 5 is best, 2 is okay, and 1 not best
+        % test a grid spacing and see how close it gets to an integer division (nice and clean)
+        commdom = m * 10^(sign(log10(extents(1,4) / i)) * floor(abs(log10(extents(1,4) / i)) + 2));
+        remainder = abs(extents(1,4) - round(extents(1,4) / i / commdom) * commdom * i);
+        
+        % attempt to find a number of divisions that comes closest to covering entire extent
+        if (remainder < lowest)
+            lowest = remainder;
+            dx = round(extents(1,4) / i / commdom) * commdom;
+            Nx = i;
+        end
+    end
+end
+
+% offset extents to have a uniform empty spacing around displaced mesh domain - this preserves scale
+offset = max(dx, abs(((extents(1,4) + 2 * dx) .* resolution / resolution(1) - extents(:,4)) / 2));
+lims(:,1) = extents(:,2) - offset;
+lims(:,2) = extents(:,3) + offset;
+
+% set up grid points in extended space and 10 more points in all directions beyond
+grid = {(round(lims(1,1) / dx) * dx - 10 * dx):dx:(round(lims(1,2) / dx) * dx + 10 * dx);
+        (round(lims(2,1) / dx) * dx - 10 * dx):dx:(round(lims(2,2) / dx) * dx + 10 * dx)};
+
+% set properties for axis object
+set(ax, 'XLim', lims(1,:), 'XTick', grid{1}, 'YLim', lims(2,:), 'YTick', grid{2}, 'Layer', 'top')
+
+%/ set gradient background
+bgx = [lims(1,1), lims(1,2), lims(1,2), lims(1,1)]; % coordinates of extended plot space vertices
+bgy = [lims(2,1), lims(2,1), lims(2,2), lims(2,2)];
+
+% I call this color scheme "Shallow Ocean" haha I'm silly XD
+cdata(1,1,:) = [0.196, 0.396, 0.608]; % bottom RGB
 cdata(1,2,:) = [0.196, 0.396, 0.608];
-cdata(1,3,:) = [0.475, 0.647, 0.827];
+cdata(1,3,:) = [0.475, 0.647, 0.827]; % top RGB
 cdata(1,4,:) = [0.475, 0.647, 0.827];
+
+% create a patch object to render gradient
 p = patch(bgx, bgy, 'k', 'Parent', ax);
 set(p, 'CData', cdata, 'FaceColor','interp', 'EdgeColor', 'none');
 uistack(p, 'bottom') % Put gradient underneath everything else
 
-
-% cdata(1,1) = min(disp_mag);
-% cdata(1,2) = min(disp_mag);
-% cdata(1,3) = max(disp_mag);
-% cdata(1,4) = max(disp_mag);
-% p = patch(bgx, bgy, 'k', 'Parent', ax);
-% set(p, 'CData', cdata, 'FaceColor','interp', 'CDataMapping', 'scaled', 'EdgeColor', 'none');
-% uistack(p, 'bottom') % Put gradient underneath everything else
-
+%// all done :)
 fprintf('Done.\n\n')
-
-
-
-
