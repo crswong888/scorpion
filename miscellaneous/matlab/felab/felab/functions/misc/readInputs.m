07ed300bdccfe62dc99cc743510bc0ec6526d6d8
crswong888 <crswong888@gmail.com>
2020-11-30 22:54:48 -0700

    this should clear the variable from the invoking workspace, not necesarily the base

--- miscellaneous/matlab/felab/felab/functions/misc/readInputs.m
+++ miscellaneous/matlab/felab/felab/functions/misc/readInputs.m
@@ -11,10 +11,10 @@ function [varargout] = readInputs(file_varnames)
     varargout = cell(1,nargout); clear_files = []; idx = 1;
     while (idx <= nargout)
         %// read and store node, element, support, and force information tables
-        file = evalin('base', file_varnames{1,idx});
+        file = evalin('caller', file_varnames{1,idx});
         varargout{idx} = readtable(file, 'ReadVariableNames', true, 'PreserveVariableNames', true);
         
         %// clear the file variables in the main workspace
         clear_files = cat(2, clear_files, 'clear ', file_varnames{1,idx}, ', '); idx = idx + 1;
-    end, evalin('base', clear_files)
+    end, evalin('caller', clear_files)
 end
