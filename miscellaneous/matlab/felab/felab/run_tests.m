1069e58064007eeb3cee93c9e93fc605402f69d0
crswong888 <crswong888@gmail.com>
2020-12-01 02:17:07 -0700

    made the regression tests script operate on gold files (TODO: figure out which variables are appropriate for comparison and to what precision)

--- miscellaneous/matlab/felab/felab/run_tests.m
+++ miscellaneous/matlab/felab/felab/run_tests.m
@@ -4,35 +4,68 @@
 %%%        crswong888@gmail.com        %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%% UNDER DEVELOPMENT: Currently, this simply runs all demo input files to ensure they run.
-%%%
-%%% TODO: This should run each demo and compare the results to an archive of verified results. This
-%%% way, when developments to functions are made, or this program is ran on a new machine, it can be
-%%% ensured that everything solves properly.
+%%% This script runs all '.m' scripts in the current directory with a corresponding '.mat' file in
+%%% a 'gold/' subdirectory and compares the results. The "gold" files serve as an archive of
+%%% verified results (regression tests) for a given demo input file. This is useful for ensuring
+%%% that everything solves as expected on new machines or after changes to the source code are made.
 
 clear all %#ok<CLALL>
-format longeng
 fprintf('\n')
 
-%// get all '.m' file info in current directory
-tests = dir('*.m');
-tests = tests(~ismember({tests.name}, 'run_tests.m')); % exclude 'run_tests.m' itself
+%// report notation for test results
+fprintf('Testing scripts against "gold" files (''OK'' = passed, ''NG'' = failed)\n')
+fprintf('--------------------------------------------------------------------------\n')
 
-%// loop over and run demo input files to ensure they complete succesfully
-for t = 1:length(tests)
+%// get all '.mat' filenames in gold directory
+gold = dir('gold/*.mat');
+
+%// loop over and run demo input files that have a matching gold workspace file
+tic % initiate test timer
+for test = 1:length(gold)
+    %/ report name of demo script currently being tested
+    currdemo = erase(gold(test).name, '.mat');
+    fprintf('%s... ', currdemo)
+    
     %/ script must run from within function so that its workspace is unique
-    runDemoScript(tests(t).name)
+    check_vars = runDemoScript([currdemo, '.m']);
+    
+    %/ compare temp file variables to those in corresponding gold archive and print results
+    status = 'OK'; % initialze a succesful test assumption
+    for var = 1:length(check_vars)
+        stringvar = check_vars{var};
+        if (~isequal(load('temp.mat', stringvar), load(['gold/', gold(test).name], stringvar)))
+            status = 'NG'; % test no good
+            break
+        end
+    end
+    
+    %/ delete temp file and report test results
+    delete temp.mat
+    fprintf([status, '\n'])
 end
 
-%// function for invoking demo scripts in a separate workspace
-function [] = runDemoScript(filename)
-    %/ invoke script
-    run(filename)
+%// report completion of testing procedure and how long it took
+fprintf('--------------------------------------------------------------------------\n')
+fprintf('Ran %d tests in %g seconds\n\n', length(gold), toc)
+
+%// function for invoking demo scripts in a separate workspace and saving results to a temp file
+function check_vars = runDemoScript(filename) %#ok<INUSD>
+    %/ invoke script and save a temporary workspace file for comparison to gold
+    clearout = evalc('run(filename)'); %#ok<NASGU>
+    clear clearout
+    save('temp')
     
-    %/ create a temporary workspace file for comparison to gold
-    save('temp.mat')
+    %/ compile list of variables subject to comparison
+    check_vars = who;
+    ignore_vars = []; % compile idices here
+    for var = 1:length(check_vars)
+        % ignore function handles
+        if (isa(eval(check_vars{var}), 'function_handle'))
+            ignore_vars = cat(1, ignore_vars, var);
+        end
+    end
+    check_vars(ignore_vars) = []; % clear variables at ignored indices
     
-    %/ close any plots generated and delete temp file
+    %/ close any plots generated
     close all
-    delete temp.mat
 end
