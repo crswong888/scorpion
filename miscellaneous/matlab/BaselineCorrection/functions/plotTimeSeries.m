b26aeee45a115d8555fc462407bebf006adfa452
crswong888 <crswong888@gmail.com>
2020-12-18 12:32:00 -0700

    I think I pretty much figured out the basic sizing formula here...

--- miscellaneous/matlab/BaselineCorrection/functions/plotTimeSeries.m
+++ miscellaneous/matlab/BaselineCorrection/functions/plotTimeSeries.m
@@ -11,15 +11,24 @@
 %%% titles not required, default plot title is series 1, 2, ...
 %%%
 %%% multilayout plot only uses 1 title
+%%%
+%%% fontsize and fontname can definitely be their own params (run on mac to test fontname)
+%%%
+%%% probably need to find way to switch back and forth on 'ActivePositionProperty' and 
+%%% 'PositionConstraint' depending on current matlab version
+
 
 function [] = plotTimeSeries(t, series, varargin)
     %// object for additional inputs which control plot behavior
     params = inputParser;
     
-    %// series must be a row vector equal in length to 't' - provide multiple by concatenating rows
+    %/ series must be a row vector equal in length to 't' - provide multiple by concatenating rows
     N = length(t);
     valid_series = @(x) validateattributes(x, {'numeric'}, {'ncols', N});
     addRequired(params, 'Series', valid_series)
+    
+    %/ font to use on plot axes and title test
+    addParameter(params, 'FontName', 'Helvetica')
     
     %// parse provided inputs
     parse(params, series, varargin{:})
@@ -28,14 +37,14 @@ function [] = plotTimeSeries(t, series, varargin)
     %%% this will be handled by a parameter
     %close all
     
-%axmargs = get(axes(figure('Visible', 'off', 'OuterPosition', pos), 'Units', 'pixels'), 'TightInset');
     
     %//
     res = get(0, 'ScreenSize');
-    pos = [res(1) + res(3) / 4, res(2) + res(4) / 2 - 3 * res(3) / 20, res(3) / 2, 3 * res(3) / 10];
+    aspect = 9 / 20 * res(3) * [1, 2 / 5];
+    pos = [res(1) + (res(3) - aspect(1)) / 2, res(2) + (res(4) - aspect(2)) / 2, aspect];
     
     %//
-    margs = [0.01, 0.02]; % minimum vertical and horizontal tight space margins
+    m = [0.01, 0.02]; % minimum vertical and horizontal tight space margins
     for p = 1:1%length(series(:,1))
         %/
         figure('OuterPosition', pos);      
@@ -43,27 +52,22 @@ function [] = plotTimeSeries(t, series, varargin)
         hold on
         
         %/
-        set(gca, 'Fontsize', 10);
-        th = title(gca, 'This Is A Plot', 'Units', 'normalized');
-        %th.Position(2) = th.Position(2) + margs(2) / 2;
+        set(gca, 'FontName', 'Helvetica', 'FontSize', 10);
+        th = title('This Is A Plot', 'Units', 'normalized', 'FontUnits', 'normalized');
+        tset = [th.Position + [0, m(2) / 2, 0], th.FontSize]; 
+        set(th, 'Position', tset(1:3), 'Units', 'data', 'FontUnits', 'points');
         xlabel('X Axis')
         ylabel('Y Axis')
         
         %/ 'TightInset' is a read-only prop - this attempts to modify it w/o knowing how it works
-        axpos = get(gca, 'Position');
-        axtight = get(gca, 'TightInset');
-        hshift = max([axpos(1) - max(axtight(1), margs(1)), axtight(1), margs(1)]) + margs(1);
-        vshift = max([axpos(2) - max(axtight(2), margs(2)), axtight(2), margs(2)]) + margs(2);
-        hstretch = max(axpos(1) + axpos(3) + min(axtight(3), margs(1)), 1 - max(axtight(3), margs(1))) - margs(1) - hshift;
-        
-        axpos(2) + axpos(4) + min(axtight(4), margs(2))
-        1 - max(axtight(4), margs(2))
-        
-        vstretch = max(axpos(2) + axpos(4) + min(axtight(4), margs(2)), 1 - max(axtight(4), margs(2))) - margs(2) - vshift;
-        %th.Position(2) = th.Position(2) + margs(2) / 2;
-        set(gca, 'Position', [hshift, vshift, hstretch, vstretch]);
-        
-        %th.Position(2) = th.Position(2) + margs(2) / 2;
+        axp = get(gca, 'Position');
+        axt = get(gca, 'TightInset');        
+        shift = [max([axp(1) - max(axt(1), m(1)), axt(1), m(1)]) + m(1),...
+                 max([axp(2) - max(axt(2), m(2)), axt(2), m(2)]) + m(2),...
+                 max(axp(1) + axp(3) + min(axt(3), m(1)), 1 - max(axt(3), m(1))) - m(1),...
+                 max(axp(2) + axp(4) + min(axt(4), m(2)), 1 - max(axt(4), m(2))) - m(2) - tset(4)];
+        shift(3:4) = shift(3:4) - shift(1:2);
+        set(gca, 'Position', shift, 'ActivePositionProperty', 'outerposition');
         
         %/
         grid on
